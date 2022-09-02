a243fbbba531712dcac3c3a7e6d24da0b6b15575
Treecie <102423723+Treecie@users.noreply.github.com>
2022-09-02 17:29:21 -0400

    commit

---
+++ src/resumeData.js
@@ -0,0 +1,131 @@
+let resumeData = {
+    "imagebaseurl": "https:/",
+    "name": "Talia Williams",
+    "role": "Full Stack Developer",
+    "linkedinId": "Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "",
+    "socialLinks": [
+      {
+        "name": "linkedin",
+        "url": "https://",
+        "className": "fa fa-linkedin"
+      },
+      {
+        "name": "github",
+        "url": "http://",
+        "className": "fa fa-github"
+      },
+      {
+        "name": "skype",
+        "url": "",
+        "className": "fa fa-twitter"
+      }
+    ],
+    "aboutme": "I am a Full Stack Developer ",
+    "address": "Dearborn Heights, MI",
+    "website": "https:// /",
+    "education": [
+      {
+        "UniversityName": "Michigan State University",
+        "specialization": "Full Stack Development",
+        "MonthOfPassing": "September",
+        "YearOfPassing": "2022",
+        "Achievements": "Certificate"
+      },
+      {
+        "UniversityName": "",
+        "specialization": "",
+        "MonthOfPassing": "",
+        "YearOfPassing": "",
+        "Achievements": ""
+      }
+    ],
+    "work": [
+      {
+        "CompanyName": "",
+        "specialization": "",
+        "MonthOfLeaving": "",
+        "YearOfLeaving": "",
+        "Achievements": ""
+        /* <li>
+          <p></p>
+        </li>
+        <li>
+          <p></p>
+        </li>
+        <li>
+          <p>
+            </p>
+        </li>
+        <li>
+      <p></p>
+        </li>
+      </ul> */
+      },
+      {
+        "CompanyName": "",
+        "specialization": "",
+        "MonthOfLeaving": "",
+        "YearOfLeaving": "",
+        "Achievements": ""
+      }
+    ],
+    "skillsDescription": "",
+    "skills": [
+      {
+        "skillname": "HTML5"
+      },
+      {
+        "skillname": "CSS"
+      },
+      {
+        "skillname": ""
+      }
+    ],
+    "portfolio": [
+      {
+        "name": "",
+        "description": "",
+        "imgurl": "./images/.png",
+        "imglink": "https://"
+      },
+      {
+        "name": "",
+        "description": "",
+        "imgurl": "./images/.png",
+        "imglink": "https://"
+  
+      },
+      {
+        "name": "",
+        "description": "",
+        "imgurl": "./images/.png",
+        "imglink": "https://"
+  
+      },
+      {
+        "name": "",
+        "description": "",
+        "imgurl": "",
+        "imglink": ""
+  
+      },{
+        "name": "",
+        "description": "",
+        "imgurl": "./images/.png",
+        "imglink": ""
+  
+      },
+      {
+        "name": "First Portfolio Page",
+        "description": "",
+        "imgurl": "./images/.png",
+        "imglink": ""
+  
+      }
+    ]
+   
+  }
+  
+  export default resumeDatas
